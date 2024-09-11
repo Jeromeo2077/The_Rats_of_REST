03e9016751c826941ff9932e2d48cffb9b92f335
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-11 15:20:13 -0600

    Created RatsSchema

--- the_rats_of_rest/src/models/Rats.js
+++ the_rats_of_rest/src/models/Rats.js
@@ -1,6 +1,8 @@
 import { Schema } from "mongoose";
 
 export const RatsSchema = new Schema({
-
-
+  name: { type: String, required: true },
+  callsign: { type: String, required: true },
+  picture: { type: String, required: true },
+  specialties: [{ type: String }]
 })
