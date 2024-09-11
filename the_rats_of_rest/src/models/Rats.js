86f472bc08679cd46197e7a36d738ff24f8527a8
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-11 16:13:42 -0600

    Added options to the RatsSchema

--- the_rats_of_rest/src/models/Rats.js
+++ the_rats_of_rest/src/models/Rats.js
@@ -1,8 +1,15 @@
-import { Schema } from "mongoose";
+import { Schema, SchemaType } from "mongoose";
 
 export const RatsSchema = new Schema({
   name: { type: String, required: true },
   callsign: { type: String, required: true },
   picture: { type: String, required: true },
   specialties: [{ type: String }]
-})
+},
+
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+
+  }
+)
